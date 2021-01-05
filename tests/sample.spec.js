a7093a5a8fd3ed18b6cc8f5e9d696100d502d599
edersonSche <eschenatto@gmail.com>
2021-01-05 17:50:36 -0300

    Add supertest and E2E tests

--- tests/sample.spec.js
+++ tests/sample.spec.js
@@ -1,5 +1,21 @@
-describe('Usuários', () => {
-  it('testar 1 + 2 + 3', () => {
-    expect(1 + 2 + 3).toBe(6);
+const app = require('../src/app');
+const request = require('supertest')(app);
+
+describe('Samples', () => {
+  it('GET samples', async () => {
+    const response = await request.get('/samples');
+    expect(response.status).toBe(200);
+  });
+
+  it('POST samples', async () => {
+    const response = await request.post('/samples').send({
+      test: 1
+    });
+    expect(response.status).toBe(200);
+  });
+
+  it('Invalid POST samples', async () => {
+    const response = await request.post('/samples').send({});
+    expect(response.status).toBe(400);
   });
 });
