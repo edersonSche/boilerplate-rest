f7a483952c6c0ee93667d3cdda75ef482d3536cc
edersonSche <eschenatto@gmail.com>
2021-01-15 11:19:20 -0300

    Add put and delete with validation and auth

--- src/modules/sample/sample.dto.js
+++ src/modules/sample/sample.dto.js
@@ -3,7 +3,14 @@ const yup = require('yup');
 module.exports = {
   post: {
     body: {
-      test: yup.number().integer().required()
+      test: yup.number().integer().defined()
+    },
+    stripUnknown: true
+  },
+
+  put: {
+    body: {
+      test: yup.number().integer().nullable()
     },
     stripUnknown: true
   }
--- src/modules/sample/sample.module.js
+++ src/modules/sample/sample.module.js
@@ -4,13 +4,13 @@ const authorizer = require('../../helpers/authorizer');
 const validator = require('../../helpers/validator');
 
 const sampleController = require('./sample.controller');
-const sampleDto = require('./sample.dto');
+const sampleDTO = require('./sample.dto');
 
 const router = express.Router();
 
 router.get('/', authorizer({ permission: 'GET_SAMPLE' }), sampleController.get);
-router.post('/', authorizer({ permission: 'POST_SAMPLE' }), validator(sampleDto.post), sampleController.post);
-router.put('/', sampleController.put);
-router.delete('/', sampleController.delete);
+router.post('/', authorizer({ permission: 'POST_SAMPLE' }), validator(sampleDTO.post), sampleController.post);
+router.put('/', authorizer({ permission: 'PUT_SAMPLE' }), validator(sampleDTO.put), sampleController.put);
+router.delete('/', authorizer({ permission: 'DELETE_SAMPLE' }), sampleController.delete);
 
 module.exports = router;
